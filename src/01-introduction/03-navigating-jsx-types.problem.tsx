d8e3a3e5b09e2a0ec48c8919a25599ee50a4ccc3
Pierre Arnaudet <pierre.arnaudet44@gmail.com>
2025-11-25 10:11:28 +0100

    introduction completed

--- src/01-introduction/03-navigating-jsx-types.problem.tsx
+++ src/01-introduction/03-navigating-jsx-types.problem.tsx
@@ -2,11 +2,12 @@ export const Component = () => {
   return (
     <div
       // How do I figure out what type aria-posinset expects?
-      aria-posinset={}
+      aria-posinset={5}
       // How do I figure out what type onChange expects?
-      onChange={}
+      onChange={() => {}}
 
       // How do I get autocomplete with JSX?
+      // ctrl + space
     />
   );
 };
